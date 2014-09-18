627bf7a2d4265dcd309a962ee64b839fb441ac64
Isabel Yepes <isabel.yepes@silveryfrog.com>
2014-09-18 10:27:47 -0500

    - Create project

---
+++ HCOTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HCOTipCalculator
+//
+//  Created by Isabel Yepes on 18/09/14.
+//  Copyright (c) 2014 Isabel Yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
