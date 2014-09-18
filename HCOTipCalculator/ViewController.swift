8f8bf953bf090f36882f134d46386363e07d69df
Isabel Yepes <isabel.yepes@silveryfrog.com>
2014-09-18 12:11:13 -0500

    - Simple Calculator finished

--- HCOTipCalculator/ViewController.swift
+++ HCOTipCalculator/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var billText: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func billEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipPercentageSelected = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billText.text).doubleValue
+        var tip = billAmount * tipPercentageSelected
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+    
+    
+    
 }
 
